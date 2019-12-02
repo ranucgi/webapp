4893aef47cb278ccd8a4b1b5a87de7d2ab85305e
ranu.patel <ranu.patel@FI-VDI-W10H0492.groupinfra.com>
2019-12-02 12:00:09 +0530

    commit first web

---
+++ springboot/src/main/java/com/example/demo/Welcome.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Welcome {
+	@RequestMapping("home")
+	@ResponseBody
+	public String show() {
+		return"welcome to boot application";
+	}
+
+}
